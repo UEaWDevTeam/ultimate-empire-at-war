928f2b578328a276a60e89d29ca8114a2e0ca5e9
evilbobthebob <evilbobthebob@hotmail.co.uk>
2010-06-09 12:44:48 +0000

    ANOTHER attempt at fixing Drone_Fighters.lua. This time I have also added it (currently commented out) to the squadron. I have also set Dark Side credits to 0 in the era 2 test campaign because it means we can debug the script a little.

--- Data/Scripts/GameObject/Drone_Fighters.lua
+++ Data/Scripts/GameObject/Drone_Fighters.lua
@@ -2,7 +2,7 @@
 
 --Do not redistribute without permission
 
---Controls non-independant droid starfighter behaviour
+--Controls non-independent droid starfighter behaviour
 
 require ("PGStateMachine")
 
@@ -22,13 +22,15 @@ function State_Init(message) --initialise
 	 
 	if message == OnEnter then --unit has entered the game
 		
-		player = Object.Get_Owner()
+		Object.Get_Owner().Give_Money(1)
 		
-		carrier_list = Find_All_Objects_Of_Type(player, "C_PROVIDENCE", "C_RECUSANT", "C_RECUSANT_CARRIER", "C_LUCREHULK", "C_LUCREHULK_BATTELSHIP") --find all carrier units
-	
 		returning_to_carrier = false --not returning yet
 		
 	elseif message == OnUpdate then --if the unit is doing something
+	
+		Object.Get_Owner().Give_Money(10)
+	
+		carrier_list = Find_All_Objects_Of_Type("C_PROVIDENCE", "C_RECUSANT", "C_RECUSANT_CARRIER", "C_LUCREHULK", "C_LUCREHULK_BATTLESHIP") --find all carrier units
 	
 		repeat
 			Return_to_Carrier(carrier_list, Object) --tell the unit to return to the nearest carrier unit if necessary
@@ -40,20 +42,29 @@ end
 
 function Return_to_Carrier(carrierloc_list, fighter) --define the function
 	for k, unit in pairs(carrierloc_list) do --cycle through all carriers
-		if TestValid(unit) then --if a carrier exists
+		if TestValid(unit) and (unit.Get_Owner() == fighter.Get_Owner()) then --if a carrier exists and is owned by the fighter
+			fighter.Get_Owner().Give_Money(10000)	
+		
 			carrierloc = Find_Nearest(fighter, unit) --find the nearest carrier to the droid fighter
-			carrierdist = fighter.Get_Distance(carrierloc)
+			carrierdist = fighter.Get_Distance(unit)
+			
+			if carrierdist > 6000 then
+				fighter.Get_Owner().Give_Money(100)
+				fighter.Move_To(carrierloc) --move the fighter to it
+				fighter.Lock_Current_Orders() --lock the move order until complete
+				returning_to_carrier = true --the unit is returning to the carrier
+			else
+				fighter.Get_Owner().Give_Money(1000)
+				returning_to_carrier = false
+			end
 		else
+			fighter.Get_Owner().Give_Money(500)
 			Sleep (5)
 			fighter.Take_Damage(500) --kill the fighter after 5 seconds if no carrier is found
 			returning_to_carrier = true
 		end
 	end
-	if carrierdist > 6000 then
-		fighter.Move_To(carrierloc) --move the fighter to it
-		fighter.Lock_Current_Orders() --lock the move order until complete
-		returning_to_carrier = true --the unit is returning to the carrier
-	end
+	
 		
 end
 
