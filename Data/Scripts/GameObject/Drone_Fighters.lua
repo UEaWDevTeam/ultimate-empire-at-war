1d46bff2ea0eb741fef8b67f3324031ab40bd64b
evilbobthebob <evilbobthebob@hotmail.co.uk>
2010-06-12 17:26:32 +0000

    Simplified Drone_Fighters.lua

--- Data/Scripts/GameObject/Drone_Fighters.lua
+++ Data/Scripts/GameObject/Drone_Fighters.lua
@@ -11,7 +11,7 @@ function Definitions ()
 
         Define_State("State_Init", State_Init)
 		
-		ServiceRate = 0.2
+		ServiceRate = 0.5
 
 end
 
@@ -22,33 +22,19 @@ function State_Init(message) --initialise
         end
 
         if message == OnEnter then --unit has entered the game
-
-                
+             
 
 		elseif message == OnUpdate then --if the unit is doing something
 			
 		        carrier_list = Find_All_Objects_Of_Type("C_PROVIDENCE") --find all carrier units
-
-				nearest_carrier = Find_Nearest(Object, "C_PROVIDENCE", PlayerObject) --find nearest player carrier unit
-				carrier_distance = Object.Get_Distance(nearest_carrier)
-				
-
 				
 			    if table.getn(carrier_list) > 0  then
-
-					--checking the distance to the next unit
-							if(carrier_distance > 2000) then
-								Object.Get_Parent_Object().Move_To(nearest_carrier) --move the fighter to it
-								Object.Lock_Current_Orders()
-							end
-
-				
-				
-				
+					
+					Find_Nearest_Carrier("C_PROVIDENCE", 2000)
+					
 				else
 				
-				Sleep(5)
-				Object.Take_Damage(1000)
+					Fighter_Killer()
 				
 				end
 
@@ -58,10 +44,22 @@ function State_Init(message) --initialise
 	
 end
 
+function Find_Nearest_Carrier(carrier_type, control_range)
 
+	nearest_carrier = Find_Nearest(Object, carrier_type, PlayerObject)
+	carrier_distance = Object.Get_Distance(nearest_carrier)
+	
+		if(carrier_distance > control_range) then
+			Object.Get_Parent_Object().Move_To(nearest_carrier) --move the fighter to it
+			Object.Lock_Current_Orders()
+		end
 
+end
+	
+function Fighter_Killer()
+	
+	Sleep(5)
+	Object.Take_Damage(1000)
 
-
-
-
+end
 
