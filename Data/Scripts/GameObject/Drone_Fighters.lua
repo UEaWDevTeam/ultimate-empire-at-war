67df620c81d1e2bc43faead11c1f17bc6b4d30b7
Shimrra <yuuzhanvong@gmx.de>
2010-06-12 15:10:52 +0000

    Add Drone Fighter script....would work IF you just use one unit class.

--- Data/Scripts/GameObject/Drone_Fighters.lua
+++ Data/Scripts/GameObject/Drone_Fighters.lua
@@ -5,12 +5,13 @@
 --Controls non-independent droid starfighter behaviour
 
 require ("PGStateMachine")
+require("PGStoryMode")
 
 function Definitions ()
 
         Define_State("State_Init", State_Init)
-
-        ServiceRate = 1 --set repeat rate of the check
+		
+		ServiceRate = 0.2
 
 end
 
@@ -22,50 +23,108 @@ function State_Init(message) --initialise
 
         if message == OnEnter then --unit has entered the game
 
-                returning_to_carrier = false --not returning yet
+                
 
-        elseif message == OnUpdate then --if the unit is doing something
+		elseif message == OnUpdate then --if the unit is doing something
+			
+				carrier_type = ""
+		        carrier_list = Find_All_Objects_Of_Type("C_PROVIDENCE") + Find_All_Objects_Of_Type("C_RECUSANT_CARRIER") + Find_All_Objects_Of_Type("C_RECUSANT")--find all carrier units
+				
+				nearest_carrier = Find_Nearest(Object, "C_PROVIDENCE", PlayerObject) --find nearest player carrier unit
+				carrier_distance = Object.Get_Distance(nearest_carrier)
+				nearest_carrier = Find_Nearest(Object, "C_RECUSANT_CARRIER", PlayerObject)
+				carrier_distance2 = Object.Get_Distance(nearest_carrier)
+				
+				--checking which unit/unit class is the nearest
+				if(carrier_distance > carrier_distance2) then
+					carrier_type="C_PPROVIDENCE"
+				else
+					carrier_distance=carrier_distance2
+					carrier_type="C_RECUSANT_CARRIER"
+				end
+				
+				nearest_carrier = Find_Nearest(Object, "C_RECUSANT", PlayerObject)
+				carrier_distance2 = Object.Get_Distance(nearest_carrier)
+				
+				if(carrier_distance > carrier_distance2) then				
+				else
+					carrier_distance=carrier_distance2
+					carrier_type="C_RECUSANT"
+				end
+				
+				--nearest_carrier = Find_Nearest(Object, "C_LUCREHULK", PlayerObject)
+				--carrier_distance2 = Object.Get_Distance(nearest_carrier)
+				
+				--if(carrier_distance > carrier_distance2) then				
+				--else
+					--carrier_distance=carrier_distance2
+					--carrier_type="C_LUCREHULK"
+				--end
+				
+				--nearest_carrier = Find_Nearest(Object, "C_LUCREHULK_CARRIER", PlayerObject)
+				--carrier_distance2 = Object.Get_Distance(nearest_carrier)
+				
+				--if(carrier_distance > carrier_distance2) then
+				--else
+					--carrier_distance=carrier_distance2
+					--carrier_type="C_LUCREHULK_CARRIER"
+				--end
+				
 
-                carrier_list = Find_All_Objects_Of_Type("C_PROVIDENCE", "C_RECUSANT", "C_RECUSANT_CARRIER", "C_LUCREHULK", "C_LUCREHULK_BATTLESHIP") --find all carrier units
+				
+			    if table.getn(carrier_list) > 0 then
+
+					--checking the distance to the next unit
+					if(carrier_type=="C_PROVIDENCE") then
+							if(carrier_distance > 2000) then
+								Object.Get_Parent_Object().Move_To(nearest_carrier) --move the fighter to it
+								Object.Lock_Current_Orders()
+							end
+					end
+				
+					if(carrier_type=="C_RECUSANT") then
+							if(carrier_distance > 3000) then
+								Object.Get_Parent_Object().Move_To(nearest_carrier) --move the fighter to it
+								Object.Lock_Current_Orders()
+							end
+					end
+				
+					if(carrier_type=="C_RECUSANT_CARRIER") then
+							if(carrier_distance > 4000) then
+								Object.Get_Parent_Object().Move_To(nearest_carrier) --move the fighter to it
+								Object.Lock_Current_Orders()
+							end
+					end
+				
+					if(carrier_type=="C_LUCREHULK") then
+							if(carrier_distance > 3500) then
+								Object.Get_Parent_Object().Move_To(nearest_carrier) --move the fighter to it
+								Object.Lock_Current_Orders()
+							end
+					end
+				
+					if(carrier_type=="C_LUCREHULK_CARRIER") then
+							if(carrier_distance > 5000) then
+								Object.Get_Parent_Object().Move_To(nearest_carrier) --move the fighter to it
+								Object.Lock_Current_Orders()
+							end
+					end
+				
+				
+				
+				else
+				
+				Object.Take_Damage(1000)
+				
+				end
+
+			
+			
+		end
 	
-                Return_to_Carrier(carrier_list)
-        end
-
 end
 
-function Return_to_Carrier(carrierloc_list)
-		 Object.Get_Owner().Give_Money(1)
-         Carrier_in_Range = false
 
-        if table.getn(carrierloc_list) > 0 then
-                 for i=0, pairs(carrierloc_list) do
-                         carrierdist = Object.Get_Distance(carrierloc_list[i])
-
-
-                         if carrierdist > 2000 then
-                         Carrier_in_Range = true
-                         Carrier_Name = carrierloc_list[i]
-                         end
-                 end
-
-        else
-				 Object.Get_Owner().Give_Money(100)	
-                 Object.Take_Damage(500)
-
-        end
-		
-		Object.Get_Owner().Give_Money(10)
-
-        if Carrier_in_Range then
-
-        else
-                 Object.Get_Parent_Object().Move_To(Carrier_Name) --move the fighter to it
-                 Object.Lock_Current_Orders() --lock the move order until complete
-
-        end
-
-
-end
 
 
 
