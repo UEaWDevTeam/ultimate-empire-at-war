bb86396f65c68ec466af6431e6d76c17580bc478
Shimrra <yuuzhanvong@gmx.de>
2010-07-07 13:38:20 +0000

    Add LS E1 Objective 2, add Bastila Hero.

---
+++ Data/Scripts/Story/Story_LS_E1_O2.lua
@@ -0,0 +1,16 @@
+-- UEAW CAMPAIGN LUA file
+
+--Do not redistribute without permission
+
+--Controls 2. objective in the campaign (capture revan)
+
+require("PGStoryMode")
+
+function Definitions()
+
+	DebugMessage("%s -- In Definitions", tostring(Script))
+	
+	StoryModeEvents = 
+	{
+	}
+end
