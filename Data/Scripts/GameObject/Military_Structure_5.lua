6fb3bf1c3744ddb430f825f910fae0351091365b
Shimrra <yuuzhanvong@gmx.de>
2010-11-24 18:33:26 +0000

    MP.

---
+++ Data/Scripts/GameObject/Military_Structure_5.lua
@@ -0,0 +1,37 @@
+--      UU    UU    EEEEEEEE     AAAA     WW   WW   WW
+--      UU    UU    EE         AA    AA   WW   WW   WW
+--      UU    UU    EEEEE      AAAAAAAA   WW   WW   WW
+--      UU    UU    EE         AA    AA    WW WWWW WW
+--       UUUUUU     EEEEEEEE   AA    AA      WW  WW
+-- © UEAW Team
+--Controls Upgrade of Structures
+
+require ("PGStateMachine")
+require("PGStoryMode")
+
+function Definitions ()
+
+	DebugMessage("%s -- In Definitions", tostring(Script))
+	Define_State("State_Init", State_Init);
+
+end
+
+function State_Init(message)
+		if message == OnEnter then
+			Planet = Object.Get_Planet_Location()
+			--Structure which has to be demolished
+			structures = Find_All_Objects_Of_Type("MILITARY_STRUCTURE_4")
+			for i, obj in pairs(structures) do
+				if(obj.Get_Planet_Location() == Planet) then
+					obj.Despawn()
+				end
+			end
+		end
+		
+		if message == OnExit then
+			Planet = Object.Get_Planet_Location()
+			Owner = Object.Get_Owner()
+			spawn_list = { "DUMMY_MILITARY_STRUCTURE" }
+			SpawnList(spawn_list,Planet,Owner,false,false)
+		end
+end
